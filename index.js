30071351f0a1c3740b7d39000f3278785abaede6
Nathan LaFreniere <quitlahok@gmail.com>
2014-02-15 15:27:36 -0800

    get some more easy commands out of the way

--- index.js
+++ index.js
@@ -11,12 +11,45 @@ function SimpleRiak(options) {
 
     this.server.host = options.host || '127.0.0.1';
     this.server.port = options.port; // we let this fall through undefined so the backend can determine its own default
+    this.bucket = options.bucket;
 
     this.backend = new backends[options.backend || 'http'](this.server);
 }
 
 SimpleRiak.prototype.getBuckets = function (callback) {
     this.backend.getBuckets(callback);
+};
+
+SimpleRiak.prototype.getBucket = function (params, callback) {
+    var bucket = params.bucket || this.bucket;
+    
+    if (!bucket) {
+        return callback(new Error('No bucket specified'));
+    }
+
+    this.backend.getBucket(bucket, callback);
+};
+
+SimpleRiak.prototype.getKeys = function (params, callback) {
+    var bucket = params.bucket || this.bucket;
+    
+    if (!bucket) {
+        return callback(new Error('No bucket specified'));
+    }
+
+    this.backend.getKeys(bucket, callback);
+};
+
+SimpleRiak.prototype.ping = function (callback) {
+    this.backend.ping(callback);
+};
+
+SimpleRiak.prototype.status = function (callback) {
+    this.backend.status(callback);
+};
+
+SimpleRiak.prototype.resources = function (callback) {
+    this.backend.resources(callback);
 };
 
 exports.createClient = function (options) {
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var client = require('./').createClient({ backend: 'http' });
+var backend = process.argv[2];
+var client = require('./').createClient({ backend: backend });
 
-client.getBuckets(function (err, buckets) {
-    console.log(buckets);
+client.resources(function () {
+    console.log(arguments);
     process.exit(0);
 });
